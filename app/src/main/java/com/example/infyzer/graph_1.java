c9a977ded3cf2d3eb5f6429ccc73f272dad92dc6
Simran Taneja <simrantaneja225@gmail.com>
2020-07-25 17:12:32 +0530

    all files

---
+++ app/src/main/java/com/example/infyzer/graph_1.java
@@ -0,0 +1,14 @@
+package com.example.infyzer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class graph_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graph_1);
+    }
+}
